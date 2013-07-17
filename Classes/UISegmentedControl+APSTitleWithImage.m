81ba0e430633235359570f4aff26cac0d1f3a7f8
Andrew Sardone <andrew@andrewsardone.com>
2013-07-17 07:13:11 -0400

    Implement helper to convert UIView to UIImage

--- Classes/UISegmentedControl+APSTitleWithImage.m
+++ Classes/UISegmentedControl+APSTitleWithImage.m
@@ -1,4 +1,28 @@
 #import "UISegmentedControl+APSTitleWithImage.h"
+#import <QuartzCore/QuartzCore.h>
+
+/**
+ * Adds the ability to grab a UIImage-representation of any UIView.
+ */
+@interface UIView (APSLayerSnapshot)
+@end
+@implementation UIView (APSLayerSnapshot)
+
+- (UIImage *)aps_imageFromLayer
+{
+    UIImage *image;
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, 0);
+    {
+        [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+        image = UIGraphicsGetImageFromCurrentImageContext();
+    }
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+@end
+
+#pragma mark -
 
 @implementation UISegmentedControl (APSTitleWithImage)
 
